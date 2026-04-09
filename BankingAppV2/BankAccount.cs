c823e1221d490e211c5ff4a9b82483b507fc19d2
Steven Riaan Piek <milkx00@gmail.com>
2026-04-09 15:01:33 +0200

    Initial commit - Banking App V2

---
+++ BankingAppV2/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class BankAccount
+{
+    public decimal Balance { get; private set; }
+
+    public List<string> History { get; private set; }
+
+    private string filePath;
+
+    public BankAccount(string userName)
+    {
+        filePath = userName.ToLower() + "_data.txt";
+        History = new List<string>();
+        LoadData();
+
+    }
+
+    public bool Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            return false;
+        }
+        Balance += amount;
+        string record = $"Deposited: {amount} on {DateTime.Now}";
+        History.Add(record);
+        SaveData();
+        return true;
+
+    }
+    public bool Withdraw(decimal amount)
+    {
+        if (amount <= 0 || amount > Balance)
+        {
+            return false;
+        }
+        Balance -= amount;
+        string record = $"Withdraw: {amount} on {DateTime.Now}";
+        History.Add(record);
+        SaveData();
+        return true;
+    }
+    public void ShowHistory()
+    {
+        foreach (string item in History)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void SaveData()
+    {
+        List<string> lines = new List<string>();
+        lines.Add(Balance.ToString());
+        lines.AddRange(History);
+
+        File.WriteAllLines(filePath, lines);
+
+    }
+    public void LoadData()
+    {
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+        var lines = File.ReadAllLines(filePath);
+        if (lines.Length > 0)
+        {
+            Balance = Convert.ToDecimal(lines[0]);
+            for (int i = 1; i < lines.Length; i++)
+            {
+                History.Add(lines[i]);
+            }
+        }
+    }
+
+
+}
+
+
---
+++ BankingAppV2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+class Program
+{
+    //I improved my banking app by introducing object-oriented design with a BankAccount class
+    //and added file handling to persist balance and transaction history between sessions.
+
+    //I moved from basic logic to OOP (classes, encapsulation)
+    //Also I added real data persistence(file storage)
+
+    static void Main()
+    {
+        BankAccount account = new BankAccount("pete");
+
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to banking app v2");
+            Console.WriteLine("1.Balance \n2. Deposit\n3. Withdraw\n4. History\n5. Exit");
+            Console.WriteLine("Choose an option");
+            string option = Console.ReadLine();
+
+            try
+            {
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine($"Balance: {account.Balance}");
+                        break;
+                    case "2":
+                        Console.WriteLine("Amount?");
+                        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+                        if (account.Deposit(deposit))
+                        {
+                            Console.WriteLine("Deposit successful");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid deposit amount");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Amount?");
+                        decimal withdraw = Convert.ToDecimal(Console.ReadLine());
+                        if (account.Withdraw(withdraw))
+                        {
+                            Console.WriteLine("Withdrawel successful");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid deposit amount");
+                        }
+
+                        break;
+                    case "4":
+                        Console.WriteLine("Transaction History:");
+                        account.ShowHistory();
+                        break;
+                    case "5":
+                        Console.WriteLine("Goodbye");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+
+                }
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+
+
